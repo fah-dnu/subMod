d46e0cbbb6335da32cac76e4c4cb8c6b6e4e0547
Javier Acevedo <fah@dnu.com.mx>
2024-03-05 16:52:33 -0600

    Añade submodulo

--- obj/Debug/net8.0/subComp.AssemblyInfo.cs
+++ obj/Debug/net8.0/subComp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("subComp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+012aff6bce6aff4655ae00828cce8bb8d1461149")]
 [assembly: System.Reflection.AssemblyProductAttribute("subComp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("subComp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
